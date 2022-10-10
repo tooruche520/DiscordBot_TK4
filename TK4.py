bef780797e723049ca5f97c30a8abc96e129c1cb
Hanchi Huang <williamhuang910520@gmail.com>
2022-10-10 19:51:49 +0800

    add !棒棒糖 command

--- TK4.py
+++ TK4.py
@@ -18,8 +18,19 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     print(f'{member} join')
-    channel = bot.get_channel(1028290028721406013)
-    await channel.send(f'{member} join')
+    channel = bot.get_channel(int(data["TK4開發測試頻道"]))
+    await channel.send(f'{member.mention} join')
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} leave')
+    channel = bot.get_channel(int(data["TK4開發測試頻道"]))
+    await channel.send(f'{member.mention} leave')
+
+@bot.command()
+async def 棒棒糖(ctx):
+    print(f'{ctx.author}給了TK4一根棒棒糖')
+    await ctx.send(f'謝謝{ctx.author.mention}的棒棒糖')
 
     
 bot.run(data['token']) 
