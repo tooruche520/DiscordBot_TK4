b028a010f306b0e5faf4ee628e399644afeae807
Hanchi Huang <williamhuang910520@gmail.com>
2022-10-10 23:46:50 +0800

    add Greeting.py
    add Greeting.py and modify TK4.py by using Cog

--- TK4.py
+++ TK4.py
@@ -1,6 +1,8 @@
 from discord.ext import commands 
 import discord
 import json
+import os
+import asyncio
 
 intents=discord.Intents.all()
 
@@ -11,29 +13,21 @@ with open('token.json', "r", encoding = "utf8") as file:
     data = json.load(file)
     token = data['token']
 
-with open('data.json', "r", encoding = "utf8") as file:
-    data = json.load(file)
-
 @bot.event
 async def on_ready():
     print("Bot in ready")
 
-@bot.event
-async def on_member_join(member):
-    print(f'{member} join')
-    channel = bot.get_channel(int(data["TK4開發測試頻道"]))
-    await channel.send(f'{member.mention} join')
 
-@bot.event
-async def on_member_remove(member):
-    print(f'{member} leave')
-    channel = bot.get_channel(int(data["TK4開發測試頻道"]))
-    await channel.send(f'{member.mention} leave')
+async def load_extensions():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
 
-@bot.command()
-async def 棒棒糖(ctx):
-    print(f'{ctx.author}給了TK4一根棒棒糖')
-    await ctx.send(f'謝謝{ctx.author.mention}的棒棒糖')
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(token)
 
-    
+asyncio.run(main())
+
 bot.run(token) 
---
+++ cogs/Greetings.py
@@ -0,0 +1,40 @@
+from discord.ext import commands 
+import discord
+import json
+
+with open('data.json', "r", encoding = "utf8") as file:
+    data = json.load(file)
+
+class Grettings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        with open('data.json', "r", encoding = "utf8") as file:
+            self.data = json.load(file)
+
+    # event
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        print(f'{member} join')
+        channel = self.bot.get_channel(int(self.data["TK4開發測試頻道"]))
+        await channel.send(f'{member.mention} join')
+
+    # event
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        print(f'{member} leave')
+        channel = self.bot.get_channel(int(self.data["TK4開發測試頻道"]))
+        await channel.send(f'{member.mention} leave')
+
+    # commands
+    @commands.command()
+    async def 棒棒糖(self, ctx):
+        print(f'{ctx.author}給了TK4一根棒棒糖')
+        await ctx.send(f'謝謝{ctx.author.mention}的棒棒糖')
+
+# 要用 async await 
+async def setup(bot):
+    await bot.add_cog(Grettings(bot))
+
+
+# bot 前面記得加 self
