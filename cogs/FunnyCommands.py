00a49a797722ab0500b0bfd816f265cf083f3802
Hanchi Huang <williamhuang910520@gmail.com>
2022-10-11 00:38:05 +0800

    Update FunnyCommands.py

--- cogs/FunnyCommands.py
+++ cogs/FunnyCommands.py
@@ -10,7 +10,7 @@ class FunnyCommands(commands.Cog):
     @commands.command()
     async def 棒棒糖(self, ctx):
         print(f'{ctx.author}給了TK4一根棒棒糖')
-        await ctx.send(f'謝謝{ctx.author.mention}的棒棒糖')
+        await ctx.send(f'謝謝 {ctx.author.mention} 的棒棒糖')
 
 # 要用 async await 
 async def setup(bot):
