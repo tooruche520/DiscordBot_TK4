d2e04b921190e82bf02ddaefbd315e37e1d2a1dc
Frank Gan <vincentgan0402@gmail.com>
2022-10-11 11:03:15 +0800

    Add on_member_ban

--- cogs/Greetings.py
+++ cogs/Greetings.py
@@ -17,14 +17,20 @@ class Grettings(commands.Cog):
     async def on_member_join(self, member):
         print(f'{member} join')
         channel = self.bot.get_channel(int(self.data["TK4開發測試頻道"]))
-        await channel.send(f'{member.mention} join')
+        await channel.send(f'恭喜{member.mention}踏入漏電的第一步 \n 請至 **#領取身分組** 領取你的身分組')
 
     # event
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        print(f'{member} leave')
-        channel = self.bot.get_channel(int(self.data["TK4開發測試頻道"]))
-        await channel.send(f'{member.mention} leave')
+        try:
+            await member.guild.fetch_ban(member)
+            print(f'{member} banned')
+            channel = self.bot.get_channel(int(data["測試頻道"]))
+            await channel.send(f'{member.mention} 受到了十萬伏特攻擊，搶救無效，被TK4送到攝影棚外面')
+        except:
+            print(f'{member} leave')
+            channel = self.bot.get_channel(int(self.data["TK4開發測試頻道"]))
+            await channel.send(f' 噢不， {member.mention} 的電漏光了')
 
     # # commands
     # @commands.command()
