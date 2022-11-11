1172ba07947e3d8edc7d95c5f930aa3a00ba8976
Harumi Tooru <tooruche520@gmail.com>
2022-11-11 21:35:31 +0800

    Create Test.py

---
+++ cogs/Test.py
@@ -0,0 +1,35 @@
+from discord.ext import commands 
+import logging as log
+import modules.MyDatabase as db
+from src.Id_collection import channle_id, emoji_list
+from cogs.LevelSystem import LevelSystem
+
+# | 指令       | 描述                    | 經驗值             | 備註                    |
+# | ---------- | ----------------------- | ------------------ | ----------------------- |
+# | !棒棒糖    | 餵 TK4 一根棒棒糖       | 增加 {N}           |                         |
+# | !晚安      | 跟 TK4 說晚安           | 增加 {N}           |                         |
+# | !尖頭拉瑞  | 對 TK4 說尖頭拉瑞       | **經驗值不會增加** | 小徹不開心，TK4也不開心 |
+# | !吃 \*食物 | 讓 TK4 幫忙送食物給小徹 | 看你給什麼         | 給喜歡的可能會加比較多  |
+# | 以下待加入 | ----------------------- | ------------------ | ----------------------- |
+# | !rua       | 摸摸TK4 uwu             | 增加 {N}           |                         |
+
+class Test(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    async def add_my_command(self, name, response, exp):
+        @commands.command(name=name)
+        async def fun(self, ctx):
+            # log.info(f'{ctx.author} 給了TK4一根棒棒糖')
+            await ctx.send(response)
+            await LevelSystem.send_level_up_message(self, db.update_user_exp(ctx.author.id, exp), ctx.author)
+        self.bot.add_command(fun)
+    
+    # commands
+    @commands.command()
+    async def add_command(self, ctx, name, response):
+        await self.cog_checkadd_my_command(name, response, 10)
+           
+# 要用 async await 
+async def setup(bot):
+    await bot.add_cog(Test(bot))
