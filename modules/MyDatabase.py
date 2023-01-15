9642938a51d5815bc59b91cdc42004d13c8d9367
晴海徹 <tooruche520@gmail.com>
2023-01-16 00:48:37 +0800

    Update MyDatabase.py

--- modules/MyDatabase.py
+++ modules/MyDatabase.py
@@ -22,6 +22,7 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS "user_exp" (
 	"adoption"	INTEGER,
 	"level"	DECIMAL,
 	"experience"	DECIMAL,
+    "twitch_id" TEXT,
 	PRIMARY KEY("id")
 )""")
 
@@ -40,7 +41,14 @@ def add_user(user):
     cursor.execute(command)
     connect.commit()
     log.debug(f"Successfully add user to database.")
-
+    
+def edit_user_twitch_account(user_id, twitch_id):
+    command = f"UPDATE user_exp SET twitch_id='{twitch_id}' WHERE user_id='{user_id}'"
+    cursor.execute(command)
+    connect.commit()
+    log.info(f"Edited user twitch account.")
+    return True
+    
 def update_user_exp(user_id, add_exp):
     if(get_limit_count(user_id) >= 5):
         log.info(f"Command limit.")
@@ -113,33 +121,34 @@ def get_user_by_userid(user_id):
     user_id = data[0][1]
     adoption = data[0][2]
     level = data[0][3]
-    experience = data[0][4]
+    experience = data[0][5]
     # print(user_id, adoption, level, experience)
     return User.User(user_id, adoption, level, experience)
 
 
-def update_user_exp_test(user, add_exp, send_level_up_message_test):
-    if(get_limit_count(user.id) >= 5):
-        log.info(f"Command limit.")
-        return False
+# def update_user_exp_test(ctx, add_exp, send_level_up_message_test):
+#     user = ctx.author
+#     if(get_limit_count(user.id) >= 5):
+#         log.info(f"Command limit.")
+#         return False
 
-    add_count(user.id)  # 增加每小時限制計數
-    command = "UPDATE user_exp "
-    user_data = get_user_by_userid(user.id)
-    if(user_data == None):
-        log.error(f"Cannot get user data from database.")
-        return False
+#     add_count(user.id)  # 增加每小時限制計數
+#     command = "UPDATE user_exp "
+#     user_data = get_user_by_userid(user.id)
+#     if(user_data == None):
+#         log.error(f"Cannot get user data from database.")
+#         return False
 
-    experience = user_data.experience + add_exp
-    level = user_data.level
-    is_upgrade = False
-    if(level != 51):
-        if(experience >= int(table_level_exp[level+1][1])):
-            level += 1
-            is_upgrade = True
+#     experience = user_data.experience + add_exp
+#     level = user_data.level
+#     is_upgrade = False
+#     if(level != 51):
+#         if(experience >= int(table_level_exp[level+1][1])):
+#             level += 1
+#             is_upgrade = True
 
-    command += f"SET level='{level}', experience='{experience}' WHERE user_id='{user_id}'"
-    # print(command)
-    cursor.execute(command)
-    connect.commit()
-    send_level_up_message_test(is_upgrade=is_upgrade, level=level, user=user)
+#     command += f"SET level='{level}', experience='{experience}' WHERE user_id='{user.id}'"
+#     # print(command)
+#     cursor.execute(command)
+#     connect.commit()
+#     asyncio.run(send_level_up_message_test(ctx, is_upgrade, level, user))
