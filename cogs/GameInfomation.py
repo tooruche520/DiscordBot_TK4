32ee9ef23c4982d040fa6f4e4512a5917b0a9b27
晴海徹 <tooruche520@gmail.com>
2023-01-16 00:44:04 +0800

    新增遊戲資訊功能

--- cogs/GameInfomation.py
+++ cogs/GameInfomation.py
@@ -9,6 +9,9 @@ from discord.utils import get
 from src.Id_collection import channle_id, emoji_list, role_list
 from dotenv import dotenv_values
 from dataclasses import dataclass
+import modules.GameInfoDatabase as game_db
+import aiohttp
+
 
 config = dotenv_values(".env")
 CHANNLE_GAME_SPLATOON = channle_id["斯普拉遁"]
@@ -24,45 +27,60 @@ class GameInfomation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.sw_game_list = []
-        self.sw_game_list.append(
-            SwGameType("splatoon3", "最新フェス情報 に第3回フェス結果を追加!", CHANNLE_GAME_SPLATOON), 
-            SwGameType("pokemon_sv", "https://appmedia.jp/pokemon_sv/76185673", CHANNLE_GAME_POKEMONSV),
+        self.sw_game_list.extend([
+            SwGameType("splatoon3", game_db.get_newest_title("splatoon3"), CHANNLE_GAME_SPLATOON),            
+            SwGameType("pokemon_sv", game_db.get_newest_title("pokemon_sv"), CHANNLE_GAME_POKEMONSV)]
         )
+        # self.sw_game_list.append(
+        #     SwGameType("splatoon3", "最新フェス情報 に第3回フェス結果を追加!", CHANNLE_GAME_SPLATOON)
+        # )
+        # self.sw_game_list.append(
+        #     SwGameType("pokemon_sv", "サーフゴーの育成論と対策 を公開", CHANNLE_GAME_POKEMONSV)
+        # )
         
-    def get_newest_data(self, sw_game):
-        response = requests.get("https://appmedia.jp/splatoon3")
-        # response = requests.get("https://appmedia.jp/pokemon_sv")
-        soup = BeautifulSoup(response.text, "html.parser")
-        node1 = soup.find(lambda tag: tag.name=="h3" and "更新記事" in tag.text).find_next_sibling().table.tbody
+    async def get_newest_data(self, sw_game):
+        url = ""
+        if (sw_game.name == "splatoon3"):
+            url = "https://appmedia.jp/splatoon3"
+        elif (sw_game.name == "pokemon_sv"):
+            url = "https://appmedia.jp/pokemon_sv"
+            
+        # response = requests.get(url)
+        # response = requests.get("https://appmedia.jp/pokemon_sv")  
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                soup = BeautifulSoup(await response.text(), "html.parser")
+                node1 = soup.find(lambda tag: tag.name=="h3" and "更新記事" in tag.text).find_next_sibling().table.tbody
 
-        result_list = node1.find_all("tr")
+                result_list = node1.find_all("tr")
 
-        for news in result_list:
-            if news.th is not None:
-                result_list.remove(news)
-                continue
-        for news in result_list:
-            if news.td.find_next_sibling().a is None:
-                result_list.remove(news)
-                continue
+                for news in result_list:
+                    if news.th is not None:
+                        result_list.remove(news)
+                        continue
+                for news in result_list:
+                    if news.td.find_next_sibling().a is None:
+                        result_list.remove(news)
+                        continue
 
-        get_data_list = []
-        for i, news in enumerate(result_list):
-            tag = news.td.div.text
-            new = news.td.find_next_sibling().a
-            if i==0:
-                newest_data_title = new.text
-            if(new.text == og_data_title):
-                if og_data_title == newest_data_title:
-                    # print("Data is newest, no print result")
-                    # return None, None, None
-                    pass
-                else:
-                    og_data_title = newest_data_title
-                break
-            else:
-                get_data_list.append(tag, new.text, (new['href']))
-        return get_data_list
+                get_data_list = []
+                for i, news in enumerate(result_list):
+                    tag = news.td.div.text
+                    new = news.td.find_next_sibling().a
+                    if i==0:
+                        newest_data_title = new.text
+                    if(new.text == sw_game.og_data_title):
+                        if sw_game.og_data_title == newest_data_title:
+                            # print("Data is newest, no print result")
+                            # return None, None, None
+                            pass
+                        else:
+                            sw_game.og_data_title = newest_data_title
+                            game_db.update_newest_title(sw_game.name, newest_data_title)
+                        break
+                    else:
+                        get_data_list.append((tag, new.text, new['href']))
+                return get_data_list
         
 
     # event
@@ -74,17 +92,19 @@ class GameInfomation(commands.Cog):
         #     SwGameType("pokemon_sv", "https://appmedia.jp/pokemon_sv/76185673", CHANNLE_GAME_POKEMONSV),
         # )
         
-        @tasks.loop(hours=8)
+        @tasks.loop(seconds=10)
         async def loop_get():
+            # print(self.sw_game_list)
             for sw_game in self.sw_game_list:
                 # role = guild.get_role(sw_game.channle_id)
-                data_list = self.get_newest_data(sw_game)
+                # print(sw_game)
+                data_list = await self.get_newest_data(sw_game)
+                channel = self.bot.get_channel(sw_game.channle_id)
                 for data in data_list:
-                    print(data[0], data[1], data[2])
-                    channel = self.bot.get_channel(sw_game.channle_id)
+                    print(sw_game.name, data[0], data[1], data[2])
                     await channel.send(f"【{data[0]}】{data[1]}\n{data[2]}")
                 
-        loop_get().start()
+        loop_get.start()
         
     
 
