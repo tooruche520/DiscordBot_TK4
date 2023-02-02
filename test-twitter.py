2ec9ea41093023e0475b80d1f7f57bcac1997a31
晴海徹 <tooruche520@gmail.com>
2023-02-02 21:56:45 +0800

    雜commit

--- test-twitter.py
+++ test-twitter.py
@@ -14,6 +14,11 @@ TWITTER_BEARER_TOKEN = config.get("TWITTER_BEARER_TOKEN")
 TWITTER_ACCESS_TOKEN = config.get("TWITTER_ACCESS_TOKEN")
 TWITTER_ACCESS_TOKEN_SECRET = config.get("TWITTER_ACCESS_TOKEN_SECRET")
 
+TAG_NEW_FURRYPIC = "#小徹在攝攝"
+TAG_NEW_INFO = "#T123"
+TAG_FANART = "#T456"
+TAG_FANART_R18 = "#T789"
+
 client = tweepy.Client(TWITTER_BEARER_TOKEN, TWITTER_API_KEY, TWITTER_ACCESS_TOKEN_SECRET, 
                      TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 
@@ -25,9 +30,34 @@ class MyStreamingClient(tweepy.asynchronous.AsyncStreamingClient):
         end_point = "https://twitter.com/twitter/status/"
         url = end_point + str(tweet.id)
         log.info(url)
+        print(tweet)
+        
+        if TAG_NEW_FURRYPIC in tweet.text:
+            self.on_new_pic(tweet)
+        elif TAG_NEW_INFO in tweet.text:
+            self.on_new_info(tweet)
+        elif TAG_FANART in tweet.text:
+            self.on_new_fanart(tweet)
+        elif TAG_FANART_R18 in tweet.text:
+            self.on_new_fanart_R18(tweet)
+            
         # Test.on_tweet_callback(url)  
         # await asyncio.sleep(5)
-
+    
+    def on_new_pic(self, tweet):
+        log.info("New PIC!!")
+    
+    def on_new_info(self, tweet):
+        log.info("New INFO!!")
+    
+    def on_new_fanart(self, tweet):
+        log.info("New FANART!!")
+        # print(tweet)
+        
+    def on_new_fanart_R18(self, tweet):
+        log.info("New R18 FANART!!")
+        # print(tweet)
+        
 stream_client = MyStreamingClient(TWITTER_BEARER_TOKEN, wait_on_rate_limit=True)
 
 async def fun():
@@ -43,7 +73,15 @@ async def fun():
 
     # 新增規則
     # 搜尋邏輯API： https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
-    await stream_client.add_rules(tweepy.StreamRule(value="from:tooruche #小徹在攝攝 -is:retweet -is:reply"))
+    rules = [
+        tweepy.StreamRule(value=f"from:tooruche {TAG_NEW_FURRYPIC} -is:retweet -is:reply"),
+        tweepy.StreamRule(value=f"from:tooruche {TAG_NEW_INFO} -is:retweet -is:reply"),
+        tweepy.StreamRule(value=f"{TAG_FANART} -is:retweet -is:reply"),
+        tweepy.StreamRule(value=f"{TAG_FANART_R18} -is:retweet -is:reply")
+    ]
+    # await stream_client.add_rules(tweepy.StreamRule(value="from:tooruche #小徹在攝攝 -is:retweet -is:reply"))
+    # await stream_client.add_rules(tweepy.StreamRule(value="from:tooruche #AAAHHA -is:retweet -is:reply"))
+    await stream_client.add_rules(add=rules)
     # 啟用
     await stream_client.filter()
     
--- test2.py
+++
@@ -1,7 +0,0 @@
-class BBB():
-    def on_tweet_received(url):
-        print("Your tweet is on this url: " + url)
-    
-
-    
-BBB.on_tweet_received("aaaa")
