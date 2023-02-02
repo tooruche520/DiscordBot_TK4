945a21ffd172135b99ed8c0727e55afcf5370eb6
晴海徹 <tooruche520@gmail.com>
2023-02-02 21:43:51 +0800

    更新指令錯誤的回應

--- cogs/ErrorMessage.py
+++ cogs/ErrorMessage.py
@@ -12,20 +12,25 @@ class ErrorMessage(commands.Cog, description="錯誤指令處理"):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
-            await ctx.send(f'窩 窩不知道這是什麼意思 {emoji_list["tc_sad"]}')
+            await ctx.send(f'TK4還小，這個指令還不太懂汪 {emoji_list[":tc_sad:"]}')
         
         elif isinstance(error, commands.MissingRole):
             if error.missing_role == ROLE_DEVELOPER:
-                await ctx.send(f'很抱歉，你沒有權限使用此指令 {emoji_list["tc_sad"]}\n若你想成為開發者貢獻一份心力，請聯絡小徹')
+                await ctx.send(f'很抱歉，你沒有權限使用此指令 {emoji_list[":tc_sad:"]}\n若你想成為開發者貢獻一份心力，請聯絡小徹')
             if error.missing_role == ROLE_HUSKY:
-                await ctx.send(f'不好意思，請不要冒充小徹壞壞 {emoji_list["tc_angry"]}\n若你想成為小徹，很抱歉TK4我也沒辦法幫你 {emoji_list["tc_sad"]}')
+                await ctx.send(f'不好意思，請不要冒充小徹壞壞 {emoji_list[":tc_angry:"]}\n若你想成為小徹，很抱歉TK4我也沒辦法幫你 {emoji_list[":tc_sad:"]}')
             
         elif isinstance(error, commands.NoPrivateMessage):
-            await ctx.send(f'你現在在私人頻道!!\n要使用該指令，請至漏電攝影棚使用 {emoji_list["tc_tongue"]}')
+            await ctx.send(f'你現在在私人頻道!!\n要使用該指令，請至漏電攝影棚使用 {emoji_list[":tc_tongue:"]}')
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f'你好像漏打了什麼東西，要不要使用 `!help` 檢查一下 {emoji_list[":tc_is_husky:"]}')
+            
         else:
-            await ctx.send(f'你要不要聽聽看你在說甚麼 {emoji_list["tc_sip"]}')
-        # await ctx.send(f'Error! {emoji_list["tc_sad"]}')
-        print(error)
+            await ctx.send(f'窩 窩不知道這是什麼意思 {emoji_list[":tc_sad:"]}')
+            await ctx.send(error)
+            
+        log.info(error)
  
     
            
